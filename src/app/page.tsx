4b21538fdd097e300ea0b49735bbcf669f755719
ammarAlzaqam <git@ammaralzaqam.com>
2025-07-07 21:08:01 +0300

    update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <section className="p-10" data-aos="zoom-in">
-      <h1 className="text-4xl font-bold">Hello, I’m Abu el-Sahab</h1>
-      <p data-aos-delay="200">I build websites and love animations.</p>
+    <section
+      className="flex justify-center items-center h-screen"
+      data-aos="zoom-in"
+    >
+      <Link href="/m&f" passHref className="max-w-[80%] md:max-w-fit">
+        <button className="text-center text-4xl font-bold border-1 border-gray-400 p-5 rounded-lg shadow-xl shadow-white cursor-pointer">
+          <p>MOHAMED</p>
+          <p>&&</p>
+          <p>FAIRUZ</p>
+        </button>
+      </Link>
     </section>
   );
 }
